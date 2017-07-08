88ce7236c779cde92e94b8ed1ada5e8558f55ab1
Manoel Vilela <manoel_vilela@engineer.com>
2017-07-07 22:59:06 -0300

    Add the basic implementation AbstractPolymorphism
    The code is not full optimized yet. On HackerRank tests this solution
    fails for big inputs, lot of set/get operations and big cache size.
    I don't know yet what exactly is happening. But probably is fault
    of a bad memory management. I didn't deallocate the Nodes on pop.
    Probably this is the cause.

--- Classes/AbstractPolymorphism.cpp
+++ Classes/AbstractPolymorphism.cpp
@@ -8,14 +8,23 @@
 #include <cassert>
 using namespace std;
 
+// https://stackoverflow.com/questions/12222417/why-should-i-initialize-member-variables-in-the-order-theyre-declared-in
+// a lista de inicialização deve serguir a ordem de criação dos membros;
 struct Node{
-    Node* next;
     Node* prev;
-    int value;
+    Node* next;
     int key;
+    int value;
     Node(Node* p, Node* n, int k, int val):prev(p),next(n),key(k),value(val){};
     Node(int k, int val):prev(NULL),next(NULL),key(k),value(val){};
+    ~Node(void);
 };
+
+Node::~Node(void) {
+    delete &this->key;
+    delete &this->value;
+    delete this;
+}
 
 class Cache{
 
@@ -29,9 +38,106 @@ protected:
 };
 
 class LRUCache: public Cache {
+private:
+    int size;
+
+    // método correto
+    // E a desalocação? Acho que vai dar segfault
+    void pop_node() {
+        // cout << "POP TAIL" << endl;
+        this->tail = this->tail->prev;
+        this->tail->next = NULL;
+        this->size -= 1;
+    }
+
+    // Essa definição é usada apenas
+    // em hit_cache para deslocar o nó pra cabeça
+    // então não é preciso desalocar memória
+    void pop_node(Node *n) {
+        // cout << "POP N" << endl;
+        if (n->prev != NULL) {
+            n->prev->next = n->next;
+        }
+        if (n->next != NULL) {
+            n->next->prev = n->prev;
+        }
+        this->size -= 1;
+    }
+
+    // Aprentemente está correto
+    void push_node(Node *head) {
+        // cout << "PUSH!" << endl;
+        if (this->size != 0) {
+            // cout << "SIZE != 0" << endl;
+            head->prev = NULL;
+            head->next = this->head;
+            this->head->prev = head;
+            this->head = head;
+        } else {
+            // cout << "SIZE == 0" << endl;
+            this->head = head;
+            this->tail = head;
+        }
+        // if overflows the capacity of Cache
+        this->size += 1;
+        if (this->size > this->cp) {
+            // cout << "FULL! POP TAIL" << endl;
+            this->pop_node();
+        }
+    }
+
+    // Se o nó não for a cabeça, move-o para a cabeça da lista.
+    void hit_cache(Node *n) {
+        if (n != this->head) {
+            pop_node(n);
+            push_node(n);
+        }
+    }
+
+
 public:
-    virtual void set()
-}
+    // não consegui usar lista de inicialização aqui
+    // porque? diz que cp não é membro LRUCache
+    LRUCache(int c) {
+        this->cp = c;
+        this->size = 0;
+    }
+    virtual void set(int key, int value) {
+        // set já tiver a key, atualizar o valor do nó
+        // e chamar hit_cache pra atualizar a posição
+        if (this->mp.count(key) > 0) {
+            this->mp[key]->value = value;
+            this->hit_cache(this->mp[key]);
+        } else {
+            // do contrário criar um novo nó e fazer push na lista
+            this->mp[key] = new Node(key, value);
+            this->push_node(mp[key]);
+        }
+    }
+    virtual int get(int key) {
+        // cout << "SEARCHING..." << endl;
+        // iterar os nós da cabeça até a cauda
+        for (Node *n = this->head; n != NULL; n = n->next){
+            // cout << "<" << n->key << "," << n->value << ">" << endl;
+            if (n->key == key) {
+                //cout << "FOUND!" << endl;
+                this->hit_cache(n);
+                return n->value;
+            }
+        }
+
+        return -1;
+    }
+
+    // função usada apenas para debug
+    void print_cache() {
+        cout << "CACHE: ";
+        for (Node *n = this->head; n != NULL; n = n->next) {
+            cout << n->key << " ";
+        }
+        cout << endl;
+    }
+};
 
 int main() {
     int n, capacity,i;
@@ -50,6 +156,7 @@ int main() {
             cin >> key >> value;
             l.set(key,value);
         }
+        l.print_cache();
     }
     return 0;
 }
